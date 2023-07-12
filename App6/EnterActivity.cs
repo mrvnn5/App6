3d2fb577dcf84dbb44d67476fd7080cd75d37e69
bulattor <bulattor@yandex.ru>
2023-07-12 04:46:52 +0300

    serverConnection

--- App6/EnterActivity.cs
+++ App6/EnterActivity.cs
@@ -5,10 +5,19 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RestSharp;
+using RestSharp.Authenticators;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Android.Provider.Contacts.Intents;
 
 namespace App6
 {
@@ -18,15 +27,71 @@ namespace App6
         private ImageButton enterButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            if (false)
+            /*Task<string> r = CheckUser();
+            r.Wait();
+            string result = r.Result;*/
+
+            var request = HttpWebRequest.Create(@"https://cdn.jsdelivr.net/gh/goodwin74/prod_rus@latest/products.json");
+            request.ContentType = "application/json";
+            request.Headers.Add("ngrok-skip-browser-warning", "1");
+            request.Method = "GET";
+            ServicePointManager.ServerCertificateValidationCallback = new
+            RemoteCertificateValidationCallback
+            (
+               delegate { return true; }
+            );
+
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
             {
-                StartActivity(typeof(MainActivity));
+                if (response.StatusCode != HttpStatusCode.OK)
+                    Console.Out.WriteLine("Error fetching data. Server returned status code: {0}", response.StatusCode);
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    var content = reader.ReadToEnd();
+                    if (string.IsNullOrWhiteSpace(content))
+                    {
+                        Console.Out.WriteLine("Response contained empty body...");
+                    }
+                    else
+                    {
+                        Toast.MakeText(BaseContext, content, ToastLength.Long).Show();
+                        Console.Out.WriteLine("Response Body: \r\n {0}", content);
+                    }
+                }
+            }
+
+
+            if (true)
+            {
+                StartActivity(typeof(RegistrationActivity));
                 OverridePendingTransition(0, 0);
             }
-            base.SetTheme(Resource.Style.AppTheme);
+            base.SetTheme(Resource.Style.AppTheme); 
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.enter_activity);
+
+            //Toast.MakeText(BaseContext, result, ToastLength.Long).Show();
         }
+
+        public async Task<string> CheckUser()
+        {
+            /*var client = new RestClient();
+            var request = new RestRequest("https://jsonplaceholder.typicode.com/posts");
+            request.RequestFormat = DataFormat.Json;
+            Toast.MakeText(BaseContext, "gjlujnjdrf", ToastLength.Long).Show();
+            var response = await client.GetAsync(request);
+            Toast.MakeText(BaseContext, "ura", ToastLength.Long).Show();
+            return response.ToString();*/
+
+            string URL = "https://jsonplaceholder.typicode.com/posts";
+
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(URL);
+
+            string result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
     }
 }
---
+++ App6/RegistrationActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App6
+{
+    [Activity(Label = "RegistrationActivity")]
+    public class RegistrationActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.registration_activity);
+            // Create your application here
+        }
+    }
+}
